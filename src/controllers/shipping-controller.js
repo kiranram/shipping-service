4ca174a9af28ea30066151e79c7fae842d30fb73
Demo User 28 <demouser28@lab-cnb.ibm.com>
2019-02-13 15:55:10 +0000

    Microservice implementation

---
+++ src/controllers/shipping-controller.js
@@ -0,0 +1,24 @@
+// src/controllers/shipping-controller.js
+
+var productService = require('../services/product-service')
+
+class ShippingController{
+
+    constructor() {
+        this.REGULAR_PRICE = 0.1
+        this.OVERNIGHT_PRICE = 1
+      }
+    
+      async getItemShipping(item) {
+        var shippingAmount = await productService.getProductWeight(item.id)
+        if (item.type.toLowerCase() === 'standard') {
+            return shippingAmount * this.REGULAR_PRICE
+        } else {
+            return shippingAmount * this.OVERNIGHT_PRICE
+        }
+        
+      }
+  
+  }
+  
+  module.exports = ShippingController
